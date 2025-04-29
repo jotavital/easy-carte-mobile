5464877d5af54516484cf28a6b736dadd585da43
joao vital <jvital@blancfashion.com>
2025-04-28 22:36:43 -0300

    feat: config tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './src/styles/global.css';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <View className="w-10 h-10 bg-blue-500" />
+            <Text className="bg-blue-500 text-blue-500">
+                Open up App.tsx to start working on your app!
+            </Text>
             <StatusBar style="auto" />
         </View>
     );
