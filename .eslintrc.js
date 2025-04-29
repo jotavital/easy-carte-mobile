cc970636952128eeba2fb14f033e271fbeaba4b8
joao vital <jvital@blancfashion.com>
2025-04-28 22:47:22 -0300

    feat: config eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    extends: ['expo', 'prettier'],
-    plugins: ['prettier'],
+    extends: ['expo', 'prettier', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['prettier', '@typescript-eslint'],
+    root: true,
     rules: {
         'prettier/prettier': 'warn',
     },
--- App.tsx
+++ App.tsx
@@ -1,24 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import './src/styles/global.css';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <View className="w-10 h-10 bg-blue-500" />
-            <Text className="bg-blue-500 text-blue-500">
-                Open up App.tsx to start working on your app!
-            </Text>
+        <View className="flex-1 justify-center items-center bg-white">
+            <Text>NICE!</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
