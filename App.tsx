201e10cd69340723e010471173757d77d31a60dc
joao vital <jvital@blancfashion.com>
2025-04-29 22:39:17 -0300

    feat: started home screen with the restaurant cards

--- App.tsx
+++ App.tsx
@@ -1,12 +1,92 @@
+// import {
+//     Roboto_100Thin,
+//     Roboto_100Thin_Italic,
+//     Roboto_300Light,
+//     Roboto_300Light_Italic,
+//     Roboto_400Regular,
+//     Roboto_400Regular_Italic,
+//     Roboto_500Medium,
+//     Roboto_500Medium_Italic,
+//     Roboto_700Bold,
+//     Roboto_700Bold_Italic,
+//     Roboto_900Black,
+//     Roboto_900Black_Italic,
+//     useFonts,
+// } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { FlatList, Image, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import './src/styles/global.css';
 
+const restaurants = [
+    {
+        id: 1,
+        name: 'Restaurant 1',
+        image: 'https://picsum.photos/seed/696/3000/2000',
+    },
+    {
+        id: 2,
+        name: 'Restaurant 1',
+        image: 'https://picsum.photos/seed/696/3000/2000',
+    },
+    {
+        id: 3,
+        name: 'Restaurant 1',
+        image: 'https://picsum.photos/seed/696/3000/2000',
+    },
+    {
+        id: 4,
+        name: 'Restaurant 1',
+        image: 'https://picsum.photos/seed/696/3000/2000',
+    },
+    {
+        id: 5,
+        name: 'Restaurant 1',
+        image: 'https://picsum.photos/seed/696/3000/2000',
+    },
+];
+
 export default function App() {
+    // useFonts({
+    //     Roboto_100Thin,
+    //     Roboto_100Thin_Italic,
+    //     Roboto_300Light,
+    //     Roboto_300Light_Italic,
+    //     Roboto_400Regular,
+    //     Roboto_400Regular_Italic,
+    //     Roboto_500Medium,
+    //     Roboto_500Medium_Italic,
+    //     Roboto_700Bold,
+    //     Roboto_700Bold_Italic,
+    //     Roboto_900Black,
+    //     Roboto_900Black_Italic,
+    // });
+
     return (
-        <View className="flex-1 justify-center items-center bg-white">
-            <Text>NICE!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView className="h-full bg-white">
+                <View className="pt-10">
+                    <View className="h-[120px]">
+                        <FlatList
+                            data={restaurants}
+                            renderItem={({ item }) => (
+                                <View className="mx-3">
+                                    <Image
+                                        src="https://picsum.photos/seed/696/3000/2000"
+                                        className="w-[90px] h-[90px] rounded-full mb-2"
+                                    />
+
+                                    <Text className="text-center">Name nice</Text>
+                                </View>
+                            )}
+                            keyExtractor={(item) => String(item.id)}
+                            horizontal
+                        />
+                    </View>
+                </View>
+
+                <StatusBar style="auto" />
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}', 'App.tsx'],
     presets: [require('nativewind/preset')],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'roboto-thin': 'Roboto_100Thin',
+                'roboto-light': 'Roboto_300Light',
+                roboto: 'Roboto_400Regular',
+                'roboto-medium': 'Roboto_500Medium',
+                'roboto-bold': 'Roboto_700Bold',
+                'roboto-black': 'Roboto_900Black',
+            },
+        },
     },
     plugins: [],
 };
