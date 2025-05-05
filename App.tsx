762e0d66b62597260f4e8bbcd6ac3f10d9a0114e
joao vital <jvital@blancfashion.com>
2025-05-05 20:55:51 -0300

    feat: improved layout of flat lists

--- App.tsx
+++ App.tsx
@@ -1,87 +1,76 @@
-// import {
-//     Roboto_100Thin,
-//     Roboto_100Thin_Italic,
-//     Roboto_300Light,
-//     Roboto_300Light_Italic,
-//     Roboto_400Regular,
-//     Roboto_400Regular_Italic,
-//     Roboto_500Medium,
-//     Roboto_500Medium_Italic,
-//     Roboto_700Bold,
-//     Roboto_700Bold_Italic,
-//     Roboto_900Black,
-//     Roboto_900Black_Italic,
-//     useFonts,
-// } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { FlatList, Image, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import './src/styles/global.css';
 
-const restaurants = [
-    {
-        id: 1,
-        name: 'Restaurant 1',
-        image: 'https://picsum.photos/seed/696/3000/2000',
-    },
-    {
-        id: 2,
-        name: 'Restaurant 1',
-        image: 'https://picsum.photos/seed/696/3000/2000',
-    },
-    {
-        id: 3,
-        name: 'Restaurant 1',
-        image: 'https://picsum.photos/seed/696/3000/2000',
-    },
-    {
-        id: 4,
-        name: 'Restaurant 1',
-        image: 'https://picsum.photos/seed/696/3000/2000',
-    },
-    {
-        id: 5,
-        name: 'Restaurant 1',
-        image: 'https://picsum.photos/seed/696/3000/2000',
-    },
-];
-
 export default function App() {
-    // useFonts({
-    //     Roboto_100Thin,
-    //     Roboto_100Thin_Italic,
-    //     Roboto_300Light,
-    //     Roboto_300Light_Italic,
-    //     Roboto_400Regular,
-    //     Roboto_400Regular_Italic,
-    //     Roboto_500Medium,
-    //     Roboto_500Medium_Italic,
-    //     Roboto_700Bold,
-    //     Roboto_700Bold_Italic,
-    //     Roboto_900Black,
-    //     Roboto_900Black_Italic,
-    // });
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakerestaurantapi.runasp.net/api/Restaurant')
+            .then((res) => res.json())
+            .then((json) => setRestaurants(json));
+    }, []);
 
     return (
         <SafeAreaProvider>
-            <SafeAreaView className="h-full bg-white">
-                <View className="pt-10">
-                    <View className="h-[120px]">
-                        <FlatList
-                            data={restaurants}
-                            renderItem={({ item }) => (
-                                <View className="mx-3">
-                                    <Image
-                                        src="https://picsum.photos/seed/696/3000/2000"
-                                        className="w-[90px] h-[90px] rounded-full mb-2"
-                                    />
+            <SafeAreaView className="h-full bg-white text-gray-700">
+                <View className="pt-10 px-5 flex gap-[2rem]">
+                    <View className="border bg-white shadow-sm border-gray-200 rounded-2xl px-2 pt-3 pb-4">
+                        <Text className="text-gray-700 font-semibold text-3xl mb-5 px-3">
+                            Mais novos
+                        </Text>
 
-                                    <Text className="text-center">Name nice</Text>
-                                </View>
+                        <View className="h-[120px] ps-3">
+                            {restaurants.length && (
+                                <FlatList
+                                    data={restaurants}
+                                    renderItem={({ item }) => (
+                                        <View className="me-3 flex items-center max-w-[90px]">
+                                            <Image
+                                                src="https://picsum.photos/seed/696/3000/2000"
+                                                className="w-[75px] h-[75px] rounded-full mb-2"
+                                            />
+
+                                            <Text className="text-center max-w-[95%]">
+                                                {item.restaurantName}
+                                            </Text>
+                                        </View>
+                                    )}
+                                    keyExtractor={(item) => String(item.restaurantID)}
+                                    horizontal
+                                />
                             )}
-                            keyExtractor={(item) => String(item.id)}
-                            horizontal
-                        />
+                        </View>
+                    </View>
+
+                    <View className="border bg-white shadow-slate-500 shadow-sm border-gray-200 rounded-2xl px-2 py-4">
+                        <Text className="text-gray-700 font-semibold text-3xl mb-5 px-3">
+                            Melhores avaliações
+                        </Text>
+
+                        <View className="h-[120px] ps-3">
+                            {restaurants.length && (
+                                <FlatList
+                                    data={restaurants}
+                                    renderItem={({ item }) => (
+                                        <View className="me-3 flex items-center max-w-[90px]">
+                                            <Image
+                                                src="https://picsum.photos/seed/696/3000/2000"
+                                                className="w-[75px] h-[75px] rounded-full mb-2"
+                                            />
+
+                                            <Text className="text-center max-w-[95%]">
+                                                {item.restaurantName}
+                                            </Text>
+                                        </View>
+                                    )}
+                                    keyExtractor={(item) => String(item.restaurantID)}
+                                    horizontal
+                                />
+                            )}
+                        </View>
                     </View>
                 </View>
 
